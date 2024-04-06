7b8740e9f4567d590e50bb1d294c1778670312fc
Qekly <benjaalejandro206@gmail.com>
2024-04-06 13:43:50 -0300

    feat: Implement utility for needed plugin constants usage

---
+++ plugin/src/main/java/me/qeklydev/clientviewer/Constants.java
@@ -0,0 +1,14 @@
+package me.qeklydev.clientviewer;
+
+public final class Constants {
+  /**
+   * The latest release on production for this plugin.
+   *
+   * @since 0.0.1
+   */
+  public static final String VERSION = "{version}";
+
+  private Constants() {
+    throw new UnsupportedOperationException("This class is for utility and cannot be instantiated.");
+  }
+}
