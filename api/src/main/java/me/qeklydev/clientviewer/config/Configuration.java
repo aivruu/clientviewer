6d4b47ea025686df515d03e92eba80549ff9a17f
Qekly <benjaalejandro206@gmail.com>
2024-04-06 13:41:44 -0300

    feat(config); Implement configuration models

---
+++ api/src/main/java/me/qeklydev/clientviewer/config/Configuration.java
@@ -0,0 +1,29 @@
+package me.qeklydev.clientviewer.config;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
+
+@ConfigSerializable
+public final class Configuration implements UsableConfiguration {
+  @Comment("""
+      The format used for the prefix.
+      This prefix is used on all plugin messages.""")
+  public String prefix = "<gradient:aqua:green>ClientViewer</gradient> <gray>>";
+
+  @Comment("""
+      If the player client provide a null brand name,
+      the player will be kicked from the server.""")
+  public boolean denyClientBrandNull = true;
+
+  @Comment("""
+      Do you want to unable that specific clients can't
+      join to the server?""")
+  public boolean blockClients = true;
+
+  @Comment("""
+      If the player client brand name equals to some
+      specified brand name, will be kicked.""")
+  public String[] blockedClients = new String[] {
+      "forge"
+  };
+}
---
+++ api/src/main/java/me/qeklydev/clientviewer/config/Messages.java
@@ -0,0 +1,45 @@
+package me.qeklydev.clientviewer.config;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+import java.util.List;
+
+@ConfigSerializable
+public final class Messages implements UsableConfiguration {
+  public String onlyPlayer = "<prefix> <red>Only players can execute this command!";
+  public String permission = "<prefix> <red>You don't have permission for this!";
+
+  public String unknownCommand = "<prefix> <red>That sub-command doesn't exist!";
+
+  public List<String> help = List.of(
+      "<gradient:aqua:green>ClientViewer</gradient> <gray>></gray> Available Commands List:",
+      "<gray> - <green><hover:show_text:''><click:run_command:'/cv help'>/clientviewer help</click></hover> <gray>Shows this messages.",
+      "<gray> - <green><hover:show_text:''><click:run_command:'/cv reload'>/clientviewer reload</click></hover> <gray>Reloads the plugin configuration.",
+      "<gray> - <green><hover:show_text:''><click:run_command:'/client'>/client [player]</click></hover> <gray>Shows your client information, or for another player.");
+
+  public String reloadSuccess = "<prefix> <red>The plugin configurations have been reloaded successful.";
+
+  public String reloadFailed = "<prefix> <red>Something went wrong during reloading process, check the console.";
+
+  public String nonClientModelInformation = "<prefix> <red>A client-model with that information was not founded.";
+
+  public String offlinePlayer = "<prefix> <red>The specified player is not connected now.";
+
+  public List<String> ownClientInformation = List.of(
+      "<gray>Showing your client information",
+      "<green> • <white>Brand name: <light_purple><brand-name>",
+      "<green> • <white>Protocol Version Number: <light_purple><protocol-number>");
+
+  public List<String> thirdPartyClientInformation = List.of(
+      "<gray>Showing client information of <yellow><player>",
+      "<green> • <white>Brand name: <light_purple><brand-name>",
+      "<green> • <white>Protocol Version Number: <light_purple><protocol-number>");
+
+  public List<String> clientBrandUnknown = List.of(
+      "<red>The brand name provided by your client is null/unknown.",
+      "<gradient:red:gold>Please try again, or use another client to join!");
+
+  public List<String> clientBrandDisallowed = List.of(
+      "<red>The client you're using has been blocked by the administrators.",
+      "<gradient:blue:red>Use another client to play on the server!");
+}
